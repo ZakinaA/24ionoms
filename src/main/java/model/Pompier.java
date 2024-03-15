f89a7272fb0096a7398e80ba8660af54708d6be5
ZakinaA <zakina.rostand@gmail.com>
2024-03-15 11:39:48 +0100

    implémentation classe métier Pompier

---
+++ src/main/java/model/Pompier.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author zakina
+ */
+public class Pompier {
+    
+    private int id;
+    private String bip ;
+    private String nom;
+    private String prenom ;
+
+    public Pompier() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBip() {
+        return bip;
+    }
+
+    public void setBip(String bip) {
+        this.bip = bip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    
+    
+}
