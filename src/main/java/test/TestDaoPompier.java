8b7908b04c1e294369ee6bd0db19d3f4ea319150
ZakinaA <zakina.rostand@gmail.com>
2024-03-15 16:41:13 +0100

    TestDaoPompier et modif de requete sql de DaoPompier.getLesPompiers en conséquence

---
+++ src/main/java/test/TestDaoPompier.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import database.ConnexionBdd;
+import database.DaoPompier;
+import java.sql.Connection;
+
+/**
+ *
+ * @author zakina
+ */
+public class TestDaoPompier {
+    
+    public static void main (String args[]){
+        
+        Connection cnx = ConnexionBdd.ouvrirConnexion();
+        System.out.println ("nombre de pomipiers=" + DaoPompier.getLesPompiers(cnx).size());
+        
+        
+    }
+    
+}
