86bc0e7808957536b84521974b23e721e0bc1f51
ZakinaA <zakina.rostand@gmail.com>
2024-03-15 11:38:15 +0100

    implémentation classe métier Caserne

---
+++ src/main/java/model/Caserne.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author zakina
+ */
+public class Caserne {
+    
+    private int id;
+    private String nom ;
+
+    public Caserne() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+    
+}
