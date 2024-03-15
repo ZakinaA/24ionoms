fa18f55e0a3576a091336e7d99b2527c3abcf4ec
ZakinaA <zakina.rostand@gmail.com>
2024-03-15 16:46:02 +0100

    TestDaoPompier et modif de requete sql de DaoPompier.getLesPompiers en conséquence

--- src/main/java/database/DaoPompier.java
+++ src/main/java/database/DaoPompier.java
@@ -26,8 +26,8 @@ public class DaoPompier {
         
         ArrayList<Pompier> lesPompiers = new ArrayList<Pompier>();
         try{
-            requeteSql = cnx.prepareStatement("select pompier.id as p_id, pompier.nom as p_nom, pompier.prenom as p_nom, c.id as c_id, c.nom as c_nom " +
-                         " from pompier inner join caserne " +
+            requeteSql = cnx.prepareStatement("select pompier.id as p_id, pompier.nom as p_nom, pompier.prenom as p_prenom, c.id as c_id, c.nom as c_nom " +
+                         " from pompier inner join caserne c " +
                          " on pompier.caserne_id = c.id ");
             resultatRequete = requeteSql.executeQuery();
             
@@ -35,7 +35,6 @@ public class DaoPompier {
                 
                 Pompier p = new Pompier();
                     p.setId(resultatRequete.getInt("p_id"));
-                    p.setBip(resultatRequete.getString("p_bip"));
                     p.setNom(resultatRequete.getString("p_nom"));
                     p.setPrenom(resultatRequete.getString("p_prenom"));
                 Caserne c = new Caserne();
