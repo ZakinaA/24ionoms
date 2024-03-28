a88cabefc7aab6fb438867b5fd22b454602b2101
WillyMonde <ts1sio@SIO301-03.sio.bts>
2024-03-28 10:41:22 +0100

    Création SousGrade et Grade

---
+++ src/main/java/model/Grade.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class Grade {
+    private int id;
+    private String libelle;
+    
+    public Grade(int id, String libelle){
+        this.id = id;
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
